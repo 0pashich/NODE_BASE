83f2f131831645eca8f0760cc57e050367a66d9d
0pashich <0pashich@gmail.com>
2021-12-14 04:36:06 +0300

    make hw3

--- index.js
+++ index.js
@@ -1,66 +1,47 @@
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
+const fs = require('fs');
+const ACCESS_LOG = './access.log';
+const { Transform } = require('stream');
 
-class Timer {
-    constructor({ index, date }) {
-        this.index = index;
-        this.date = date;
+const IP_ARR = [
+    '89.123.1.41',
+    '34.48.240.111'
+]
+const readStream = fs.createReadStream(ACCESS_LOG,
+    {
+        flags: 'r',
+        encoding: 'utf-8',
+        // autoClose
+        // start
+        // end
+        // highWaterMark: 164,
     }
-}
-
-const generateNewTimer = (index, arg) => {
-    const argArr = arg.split('-');
-    const date = Date.parse(`${argArr[3]}-${argArr[2]}-${argArr[1]}T${argArr[0]}:00:00`);
-    return new Timer({ index, date });
-}
+);
 
 
+IP_ARR.forEach(ip => {
 
-const intervalHendler = (timerArr) => {
-    emitter.emit('clear',);
-    let now = Date.now();
-    timerArr.forEach((element) => {
-        if (now < element.date) {
-            emitter.emit('event', now, element);
-        } else {
-            emitter.emit('end', element);
+    const writeStream = fs.createWriteStream(
+        `${ip}_requests.log`,
+        {
+            encoding: 'utf-8',
+            flags: 'a',
         }
-    })
-}
+    );
+    const regexp = new RegExp(`^${ip}.*$`, 'gm')
+    var lastStingOfChunk = '';
 
+    const tStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const chunkWithoutLastString = chunk.toString().match(/(.*)\n/gs).join();
+            const transformedChunk = (lastStingOfChunk + chunkWithoutLastString).match(regexp) ? (lastStingOfChunk + chunkWithoutLastString).match(regexp).join('\n') + '\n' : '';
+            lastStingOfChunk = chunk.toString().match(/\n(.*$)/)[1];
+            this.push(transformedChunk);
 
-if (process.argv.length < 3) {
-    emitter.emit('error', 'Не задано ни одного таймера.')
-} else {
-
-    let timerArr = [];
-    let argumentCheck = /^\d\d-\d\d-\d\d-\d\d\d\d$/;
-
-    for (let i = 2; i <= process.argv.length - 1; i++) {
-        argumentCheck.lastIndex = 0
-        if (argumentCheck.test(process.argv[i])) {
-            timerArr.push(generateNewTimer(i - 1, process.argv[i]));
-        } else {
-            console.log(`Аргумент ${i - 1} имеет неверный формат`);
-
+            callback();
         }
-    }
+    });
 
-    setInterval(intervalHendler, 1000, timerArr);
-}
-
-emitter.on('event', (nowDate, timer) => {
-    let diff = timer.date - nowDate;
-    let daysDiff = Math.floor(diff / 1000 / 60 / 60 / 24);
-    diff -= daysDiff * 1000 * 60 * 60 * 24;
-    let hourDiff = Math.floor(diff / 1000 / 60 / 60);
-    diff -= hourDiff * 1000 * 60 * 60;
-    let minDiff = Math.floor(diff / 1000 / 60);
-    diff -= minDiff * 1000 * 60;
-    let secDiff = Math.floor(diff / 1000);
-    console.log(`Таймер ${timer.index}: ${daysDiff} дней ${hourDiff}:${minDiff}:${secDiff}`)
-});
-
-emitter.on('clear', () => console.clear())
-emitter.on('end', (timer) => console.log(`Таймер ${timer.index}: Время вышло`))
-emitter.on('error', console.log);
+    readStream
+        .pipe(tStream)
+        .pipe(writeStream);
+})
