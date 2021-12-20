ccefb2b68fe1ce11f6bc952441d706b95ac60f19
0pashich <0pashich@gmail.com>
2021-12-21 02:43:58 +0300

    clear code

--- index.js
+++ index.js
@@ -2,42 +2,12 @@
 const http = require("http");
 const fs = require('fs');
 const url = require('url');
-
-// const yargs = require('yargs');
 const path = require('path');
-// const inquirer = require('inquirer');
 
 
 const host = 'localhost';
 const port = 8000;
 const dir = process.cwd();
-
-// const options = yargs
-//     .usage('Usage: -p <path to the file> -s <search string>')
-//     .option({
-//         'path': {
-//             alias: 'p',
-//             describe: 'Path to file',
-//             type: 'string'
-//         },
-//         'search': {
-//             alias: 's',
-//             describe: 'Search string in file',
-//             type: 'string'
-//         }
-//     }
-//     ).argv;
-
-// let executionDir = process.cwd();
-// if (options.path != null) { executionDir = options.path }
-// fileObjs = fs.readdirSync(__dirname, { withFileTypes: true });
-// console.log(fileObjs);
-
-// console.log("\nCurrent directory files:");
-// fileObjs.forEach(file => {
-//     console.log(file.name, file.isDirectory());
-// });
-
 
 
 const isDir = (filename) => fs.lstatSync(filename).isDirectory();
@@ -45,52 +15,10 @@ const fileList = (dir) => {
     return ['../', ...fs.readdirSync(dir, { withFileTypes: true }).map(file => file.isDirectory() ? file.name + '/' : file.name)];
 }
 
-// console.log(fileList(__dirname));
-
-// const run = (dir) => {
-//     inquirer
-//         .prompt([
-//             {
-//                 type: 'list',
-//                 name: 'file',
-//                 message: `${dir}: choose a file`,
-//                 choices: fileList(dir),
-//                 loop: false,
-//                 pageSize: 10
-//             }
-//         ])
-//         .then(({ file }) => {
-//             const fullPath = path.join(dir, file);
-//             if (isDir(fullPath)) { run(fullPath) }
-//             else {
-//                 const data = fs.readFileSync(fullPath, 'utf-8');
-//                 if (options.search != null) {
-//                     const regexp = new RegExp(`^.*(${options.search}).*$`, 'gm');
-//                     const dataSearch = data.match(regexp);
-//                     console.log(dataSearch.join('\n'));
-//                     console.log('найдено совпадений: ', dataSearch.length )
-//                 } else {
-//                     console.log(data);
-//                 }
-
-//             }
-//         });
-// }
-
-// run(executionDir);
-// let fullPath = dir;
-// let subPath = './';
-
 const requestListener = (req, res) => {
     if (req.method === 'GET') {
         try {
             const { pathname } = url.parse(req.url, false);
-            console.log(pathname);
-            // console.log(req.url)
-            // if (pathname != null) {
-            //     fullPath = path.join(fullPath, String(pathname));
-            //     subPath = path.join(subPath, String(pathname));
-            // }
             if (isDir('.' + pathname)) {
                 const filenames = fileList('.' + pathname);
                 res.writeHead(200, {
@@ -115,8 +43,6 @@ const requestListener = (req, res) => {
                 res.writeHead(200);
                 readStream.pipe(res);
             }
-
-
         } catch (error) {
             console.log(error);
             res.writeHead(500);
